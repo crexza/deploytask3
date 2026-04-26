793155683392f06499fba8955b64a753047072d1
crexza <caressa2004@gmail.com>
2026-04-27 00:09:49 +0800

    connect database

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from Render auto Deploy! UPDATED");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-app.listen(3000, () => {
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send("Database Connected ✔ Time: " + result.rows[0].now);
+  } catch (err) {
+    res.send("DB Connection Failed ❌ " + err.message);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log("Server running");
 });
